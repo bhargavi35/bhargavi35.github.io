61239ab4ea15cda18a2c51a6480c493b61c47393
bhargavi35 <bhargavibanu3513@gmail.com>
2022-11-25 17:40:20 +0530

    edu respon

--- js/dark.js
+++ js/dark.js
@@ -14,3 +14,27 @@ window.addEventListener("load", () => {
 
     }
 })
+
+
+// submit to mail
+
+function my() {
+    alert("Message Sent Successfully!! Thank You")
+}
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "username",
+        Password: "password",
+        To: 'chellabhargavi53@gmail.com',
+        From: document.getElementById("email"),
+        Body: "Name: " + document.getElementById("name").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Subject: " + document.getElementById("body").value
+            + "<br> Message: " + document.getElementById("message").value
+
+    }).then(
+        message => alert("Message sent Succesfully,Thank You!")
+        // window.location("index.html")
+    );
+}
