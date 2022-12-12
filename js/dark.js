b5c6476cd0dee77bc52a5d675e4112adb95b655d
bhargavi35 <bhargavibanu3513@gmail.com>
2022-12-12 22:25:31 +0530

    contact fun

--- js/dark.js
+++ js/dark.js
@@ -17,24 +17,48 @@ window.addEventListener("load", () => {
 
 
 // submit to mail
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        sub: document.getElementById("sub").value,
+        message: document.getElementById("message").value,
+    };
 
-function my() {
-    alert("Message Sent Successfully!! Thank You")
-}
-function sendEmail() {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "username",
-        Password: "password",
-        To: 'chellabhargavi53@gmail.com',
-        From: document.getElementById("email"),
-        Body: "Name: " + document.getElementById("name").value
-            + "<br> Email: " + document.getElementById("email").value
-            + "<br> Subject: " + document.getElementById("body").value
-            + "<br> Message: " + document.getElementById("message").value
+    const serviceID = "service_f3nen9s";
+    const templateID = "template_rsrfngi";
 
-    }).then(
-        message => alert("Message sent Succesfully,Thank You!")
-        // window.location("index.html")
-    );
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("sub").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully!!")
+
+        })
+        .catch(err => console.log(err));
+
 }
+
+// function my() {
+//     alert("Message Sent Successfully!! Thank You")
+// }
+// function sendEmail() {
+//     Email.send({
+//         Host: "smtp.gmail.com",
+//         Username: "username",
+//         Password: "password",
+//         To: 'chellabhargavi53@gmail.com',
+//         From: document.getElementById("email"),
+//         Body: "Name: " + document.getElementById("name").value
+//             + "<br> Email: " + document.getElementById("email").value
+//             + "<br> Subject: " + document.getElementById("body").value
+//             + "<br> Message: " + document.getElementById("message").value
+
+//     }).then(
+//         message => alert("Message sent Succesfully,Thank You!")
+//         // window.location("index.html")
+//     );
+// }
